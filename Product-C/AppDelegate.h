3cff50af072d8280e327d106c59b9ee630be4ea8
UntilYou-QC <578184833@qq.com>
2016-07-15 11:02:17 +0800

    Initial Commit

---
+++ Product-C/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Product-C
+//
+//  Created by UntilYou-QC on 16/7/15.
+//  Copyright © 2016年 UntilYou-QC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
